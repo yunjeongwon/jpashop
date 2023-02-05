f766d3550ab6626c5e66b7ccd4378e59a1d1c80c
윤정원 <yunjwon94@gmail.com>
2023-02-05 23:20:24 +0900

    상품 등록

---
+++ src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter @Setter
+public class BookForm {
+
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+    private String author;
+    private String isbn;
+}

b4b82de2808e1166effed37e61fcd682d324015e
윤정원 <yunjwon94@gmail.com>
2023-02-05 23:24:04 +0900

    상품 조회

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -35,5 +37,12 @@ public class ItemController {
         itemService.saveItem(book);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
     }
 }
